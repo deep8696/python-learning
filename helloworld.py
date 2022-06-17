07f1df81044ff47b27bd36961b3a68f29a0796d5
deep8696 <pateldeep8696@gmail.com>
2022-06-17 12:27:29 -0400

    print, comments, variable

---
+++ helloworld.py
@@ -0,0 +1,11 @@
+print("Hello World!!!!!!!!")
+print("This is my first program in Python")
+
+print("........Python Syntax.........")
+if 9>4 :
+    print("9 is greater than 4")
+if 9<4 :
+    print("9 is greater than 4")
+
+print("........comments.........")
+#this is how we can comment using # in start of line
