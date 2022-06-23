2fef1d6eed752f09f93f478b1d127cae3761929d
deep8696 <pateldeep8696@gmail.com>
2022-06-23 10:48:28 -0400

    variables

---
+++ variables.py
@@ -0,0 +1,34 @@
+#python variables
+x = 5
+y = "Deep Patel"
+print(x)
+print(y)
+
+#casting 
+
+x = str(8696)
+y = int(8696)
+z = float(8696)
+
+print(x)
+print(y)
+print(z)
+
+
+#get data types 
+
+x = 5 
+y = "Deep Patel"
+z = 26.08
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+#print multiple items
+
+x, y, z = "Deep", "xyz", "abc"
+print(x)
+print(y)
+print(z)
